e1b003a6045171f5261ec9390904f34453c31a99
GaborIreHun <gabor.sebestyen.eire.com>
2023-04-27 18:24:19 +0100

    2

--- src/main/java/com/tus/flight/FlightManagerApplication.java
+++ src/main/java/com/tus/flight/FlightManagerApplication.java
@@ -2,10 +2,13 @@ package com.tus.flight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FlightManagerApplication {
 
 	@Bean
