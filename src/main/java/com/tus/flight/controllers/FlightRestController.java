a9203f146fecb8cfce120108affd42194216f37d
GaborIreHun <gabor.sebestyen.eire.com>
2023-04-28 19:30:02 +0100

    validation and commenting

--- src/main/java/com/tus/flight/controllers/FlightRestController.java
+++ src/main/java/com/tus/flight/controllers/FlightRestController.java
@@ -5,13 +5,11 @@ import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
-import javax.validation.constraints.DecimalMin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
